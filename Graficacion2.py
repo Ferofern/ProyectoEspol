111af8dd22cf264f3f11b63c06fa44f64c0222b0
Felix Romero <ferofern@espol.edu.ec>
2025-07-17 20:53:51 -0500

    Update Graficacion2.py

--- Graficacion2.py
+++ Graficacion2.py
@@ -1,5 +1,6 @@
 from manim import *
 
+# Comentarios generados por copilot para futura modificacion por parte de un tercero
 
 class Toma1(ThreeDScene):
     def construct(self):
@@ -250,12 +251,52 @@ class Toma7(ThreeDScene):
         self.wait(2)
 
 
-class Toma8(ThreeDScene):
+class Toma8(Scene):
     def construct(self):
-        f1 = MathTex(
-            r"grafico",
-            font_size=42
-        )
+        linea_h = Line(LEFT * 6, RIGHT * 6)
+        self.add(linea_h)
+
+        linea_0 = DashedLine(UP * 3 + ORIGIN, DOWN * 3 + ORIGIN)
+        linea_1 = DashedLine(UP * 3 + RIGHT * 4, DOWN * 3 + RIGHT * 4)
+        self.add(linea_0, linea_1)
+        p1 = MathTex(r"(", font_size=50).move_to(LEFT * 6 + UP * 0.05)
+        p2 = MathTex(r")", font_size=50).move_to(LEFT * 0.05 + UP * 0.05)
+
+        p3 = MathTex(r"(", font_size=50).move_to(RIGHT * 0.06 + UP * 0.05)
+        p4 = MathTex(r")", font_size=50).move_to(RIGHT * 3.95 + UP * 0.05)
+
+        p5 = MathTex(r"(", font_size=50).move_to(RIGHT * 4.05 + UP * 0.05)
+        p6 = MathTex(r")", font_size=50).move_to(RIGHT * 6 + UP * 0.05)
+        self.add(p1, p2, p3, p4, p5, p6)
+
+        txt_menos_inf = MathTex(r"-\infty", font_size=30).move_to(LEFT * 6 + DOWN * 0.3)
+        txt_0 = MathTex(r"0", font_size=30).move_to(RIGHT * 0.2 + DOWN * 0.3)
+        txt_1 = MathTex(r"1", font_size=30).move_to(RIGHT * 4.3 + DOWN * 0.3)
+        txt_mas_inf = MathTex(r"+\infty", font_size=30).move_to(RIGHT * 6.8 + DOWN * 0.3)
+        self.add(txt_menos_inf, txt_0, txt_1, txt_mas_inf)
+
+        f1 = MathTex(r"f\:'\left(-1\right)\:=\:-2e^{-1}\:<\:0", font_size=36).shift(LEFT * 4 + UP * 2.5)
+        f1abajo1 = MathTex(r"f'(x) < 0", font_size=32).next_to(f1, DOWN, buff=0.6).align_to(f1, LEFT)
+
+        f2 = MathTex(r"f'\left(\frac{1}{2}\right) < 0", font_size=36).next_to(f1, RIGHT, buff=3).shift(DOWN * -0.1)
+        f2abajo2 = MathTex(r"f'(x) < 0", font_size=32).next_to(f2, DOWN, buff=0.5).align_to(f2, LEFT)
+
+        f3 = MathTex(r"f'(2) > 0", font_size=36).next_to(f2, RIGHT * 0.57, buff=3).shift(DOWN * -0.1)
+        f3abajo3 = MathTex(r"f'(x) > 0", font_size=32).next_to(f3, DOWN, buff=0.6).align_to(f3, LEFT)
+
+        texto_final = MathTex(r"f \text{ es estrictamente decreciente en } (-\infty,0)", font_size=36).to_edge(DOWN)
+
+        self.play(Write(f1), Write(f1abajo1))
+        self.wait(1.5)
+
+        self.play(Write(f2), Write(f2abajo2))
+        self.wait(1.5)
+
+        self.play(Write(f3), Write(f3abajo3))
+        self.wait(1.5)
+
+        self.play(Write(texto_final))
+        self.wait(2)
 
 
 class Toma9(ThreeDScene):
@@ -309,16 +350,112 @@ class Toma9(ThreeDScene):
         self.wait(2)
 
 
-class toma10(ThreeDScene):
+class Toma10(Scene):
     def construct(self):
-        f0 = MathTex(
-            r"(grafico)",
+        linea_h = Line(LEFT * 6, RIGHT * 6)
+        self.add(linea_h)
+        linea_0 = DashedLine(UP * 3 + ORIGIN, DOWN * 2 + ORIGIN)
+        self.add(linea_0)
+        txt_0 = MathTex(r"0", font_size=30).move_to(RIGHT * 0.2 + DOWN * 0.3)
+        self.add(txt_0)
+        p1 = MathTex(r")", font_size=50).move_to(LEFT * 0.05 + UP * 0.05)
+        p2 = MathTex(r"(", font_size=50).move_to(RIGHT * 0.06 + UP * 0.05)
+        self.add(p1, p2)
+        txt_menos_inf = MathTex(r"-\infty", font_size=30).move_to(LEFT * 6 + DOWN * 0.3)
+        txt_mas_inf = MathTex(r"\infty", font_size=30).move_to(RIGHT * 6 + DOWN * 0.3)
+        self.add(txt_menos_inf, txt_mas_inf)
+
+        f1 = MathTex(r"f\:''\left(-1\right)\:=\:-5e^{-1}\:<\:0", font_size=36).shift(LEFT * 3 + UP * 2.5)
+        f1abajo1 = MathTex(r"f''(x) < 0", font_size=32).next_to(f1, DOWN, buff=0.6).align_to(f1, LEFT)
+
+        f2 = MathTex(r"f\:''\left(1\right)\:=\:e>0", font_size=36).next_to(f1, RIGHT, buff=3).shift(DOWN * -0.1)
+        f2abajo2 = MathTex(r"f''(x) > 0", font_size=32).next_to(f2, DOWN, buff=0.5).align_to(f2, LEFT)
+
+
+        self.play(Write(f1), Write(f1abajo1))
+        self.wait(1.5)
+
+        self.play(Write(f2), Write(f2abajo2))
+        self.wait(1.5)
+
+        texto_concava_abajo = MathTex(r"f \text{ es cóncava hacia abajo en } (-\infty,0)", font_size=36).to_edge(DOWN * 2.5)
+        texto_concava_arriba = MathTex(r"f \text{ es cóncava hacia arriba en } (0,\infty)", font_size=36).to_edge(DOWN).shift(DOWN * 0.1)
+
+        self.play(Write(texto_concava_abajo))
+        self.wait(2)
+
+        self.play(Write(texto_concava_arriba))
+        self.wait(2)
+
+class Toma11(Scene):
+    def construct(self):
+        f1 = MathTex(
+            r"(vi)\quad f\ \text{no tiene puntos de inflexión}",
             font_size=42
         )
+        f1.move_to(ORIGIN)
+        self.play(Write(f1))
+        self.wait(2)
 
 
+class Toma12(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[-4, 4, 1],
+            y_range=[-2, 7, 1],
+            x_length=10,
+            y_length=6,
+            axis_config={"include_tip": True},
+            x_axis_config={"numbers_to_include": [-3, -2, -1, 0, 1, 2, 3]},
+            y_axis_config={"numbers_to_include": [1, 2, 3, 4, 5, 6]},
+        )
 
+        labels = axes.get_axis_labels(x_label="x", y_label="y")
 
+        graph = axes.plot(
+            lambda x: np.exp(x) / x,
+            x_range=[-4, -0.1, 0.01],
+            color=BLUE
+        )
+        graph2 = axes.plot(
+            lambda x: np.exp(x) / x,
+            x_range=[0.1, 4, 0.01],
+            color=BLUE
+        )
 
+        dashed_y_axis = DashedLine(
+            start=axes.c2p(0, -10),
+            end=axes.c2p(0, 6.5),
+            color=RED
+        )
 
-# \text{}
+        dashed_x1_up = DashedLine(
+            start=axes.c2p(1, 1.2),
+            end=axes.c2p(1, 7),
+            color=RED
+        )
+        dashed_x1_down = DashedLine(
+            start=axes.c2p(1, -2),
+            end=axes.c2p(1, 0.8),
+            color=RED
+        )
+
+        txt_izq_arriba = Tex("decreciente", font_size=30).move_to(axes.c2p(-2.2, 1))
+        txt_izq_abajo = Tex("cóncava hacia abajo", font_size=30).move_to(axes.c2p(-2.2, -2.5))
+
+        txt_centro_arriba = Tex("decreciente", font_size=30).move_to(axes.c2p(0.75, 1))
+        txt_centro_abajo = Tex("cóncava hacia arriba", font_size=30).move_to(axes.c2p(2.2, -2.5))
+
+        txt_der_arriba = Tex("creciente", font_size=30).move_to(axes.c2p(2.5, 1))
+
+        self.play(Create(axes), Write(labels))
+        self.play(Create(graph), Create(graph2))
+        self.play(Create(dashed_y_axis), Create(dashed_x1_up), Create(dashed_x1_down))
+        self.play(
+            Write(txt_izq_arriba),
+            Write(txt_izq_abajo),
+            Write(txt_centro_arriba),
+            Write(txt_centro_abajo),
+            Write(txt_der_arriba),
+        )
+        self.wait(3)
